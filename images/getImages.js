ae8acfe486a5b8f1cecf6fbba5c540481c853d07
Vilzz <n79613928275@gmail.com>
2021-04-03 10:37:32 +0400

    Image size check helper added

--- images/getImages.js
+++ images/getImages.js
@@ -7,7 +7,7 @@ dotenv.config({ path: './config/config.env' })
 const downloadImage = async (link, filename) => {
   const __dirname = process.cwd()
   const url = `${process.env.DONOR_URL}${link}`
-  const path = Path.resolve(__dirname, './images/small', `${filename}.jpg`)
+  const path = Path.resolve(__dirname, './images/big', `${filename}.jpg`)
   const writer = fs.createWriteStream(path)
 
   const response = await axios({
